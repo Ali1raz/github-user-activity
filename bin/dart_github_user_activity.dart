c04712664a3694ff5e699665418f43dc7aa128a0
Ali Raza <fa22-bse-118@cuivehari.edu.pk>
2025-04-13 10:00:51 +0500

    basic GET

--- bin/dart_github_user_activity.dart
+++ bin/dart_github_user_activity.dart
@@ -1,5 +1,21 @@
-import 'package:dart_github_user_activity/dart_github_user_activity.dart' as dart_github_user_activity;
+import 'dart:convert';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_github_user_activity.calculate()}!');
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+
+  try {
+    final resp = await http.get(url);
+    if (resp.statusCode == 200) {
+      List<dynamic> data = jsonDecode(resp.body);
+      for (var item in data.take(5)) {
+        print("Title: ${item['title']}");
+      }
+    } else {
+      print("Error: ${resp.statusCode}");
+    }
+  } catch (e) {
+    print("Error: $e");
+  }
 }
