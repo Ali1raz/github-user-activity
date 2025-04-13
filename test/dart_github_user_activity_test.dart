e2f949ffdeb376a4d43b7f8419e9ae43a64257d3
Ali Raza <fa22-bse-118@cuivehari.edu.pk>
2025-04-13 08:44:52 +0500

    init project

---
+++ test/dart_github_user_activity_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_github_user_activity/dart_github_user_activity.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
