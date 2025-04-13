1662bf4c5121f150e2900d292968822dfe54d1fd
Ali Raza <fa22-bse-118@cuivehari.edu.pk>
2025-04-13 10:38:42 +0500

    CLI args

--- README.md
+++ README.md
@@ -26,3 +26,31 @@ final url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
     print("Error: $e");
   }
 ```
+
+## CLI args:
+
+```dart
+void main(List<String> args) async {
+  if (args.isEmpty || args.length > 1) {
+    print("Please provide a single argument.");
+    print("Please provide posts count.");
+    print("Usage: dart run bin/dart_github_user_activity.dart 3");
+    return;
+  }
+
+  // ...
+  final postsCount = int.tryParse(args[0]);
+  print("You wanted $postsCount posts.");
+
+  if (postsCount == null) {
+    print("Please provide a valid number.");
+    return;
+  }
+
+  // ...
+
+  for (var item in data.take(postsCount)) {
+        print("Title: ${item['title']}");
+      }
+}
+```
--- bin/dart_github_user_activity.dart
+++ bin/dart_github_user_activity.dart
@@ -2,14 +2,37 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-void main() async {
+void main(List<String> args) async {
+  if (args.isEmpty || args.length > 1) {
+    print("Please provide a single argument.");
+    print("Please provide posts count.");
+    print("Usage: dart run bin/dart_github_user_activity.dart 3");
+    return;
+  }
+
   final url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+
+  final postsCount = int.tryParse(args[0]);
+  print("You wanted $postsCount posts.");
+
+  if (postsCount == null) {
+    print("Please provide a valid number.");
+    return;
+  }
+  if (postsCount > 7) {
+    print("Cant be > 7.");
+    return;
+  } else if (postsCount < 1) {
+    print("Cant be < 1.");
+    return;
+  }
+  print("Fetching posts...");
 
   try {
     final resp = await http.get(url);
     if (resp.statusCode == 200) {
       List<dynamic> data = jsonDecode(resp.body);
-      for (var item in data.take(5)) {
+      for (var item in data.take(postsCount)) {
         print("Title: ${item['title']}");
       }
     } else {
