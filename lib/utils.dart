d285385e3a2ad1bce4eee3336feedf1535d66b42
Ali Raza <fa22-bse-118@cuivehari.edu.pk>
2025-04-13 11:24:22 +0500

    printActivity

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-void fetchUserActivuty(String username) async {
+Future<void> fetchUserActivuty(String username) async {
   final url = Uri.parse("https://api.github.com/users/$username/events");
   try {
     final res = await http.get(url);
@@ -10,9 +12,17 @@ void fetchUserActivuty(String username) async {
       return;
     }
 
-    print("Fetching $username's activity...");
+    print("Fetching $username's activity...\n");
     if (res.statusCode == 200) {
-      print("User $username found.");
+      final data = jsonDecode(res.body);
+      for (var event in data) {
+        if (data is List && data.isNotEmpty) {
+          final type = event['type'];
+          final repo = event['repo']['name'];
+          final createdAt = event['created_at'];
+          printUserActivity(username, "$type on $repo at $createdAt");
+        }
+      }
     } else {
       print("Error: ${res.statusCode}");
     }
@@ -20,4 +30,8 @@ void fetchUserActivuty(String username) async {
     print("Error: $e");
     return;
   }
+}
+
+void printUserActivity(String username, String activity) {
+  print("$username's activity: $activity");
 }
